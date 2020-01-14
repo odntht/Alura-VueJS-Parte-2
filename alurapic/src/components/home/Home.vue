460a698643266731c7db3c7823f5877c790dfa5b
Fernando <odntht@gmail.com>
2020-01-14 16:25:08 -0300

    UPDATE - Finalizando Aula 1 (Diretivas)

--- alurapic/src/components/home/Home.vue
+++ alurapic/src/components/home/Home.vue
@@ -8,7 +8,7 @@
 
         <meu-painel :titulo="foto.titulo">
           
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+          <imagem-responsiva v-meu-transform:scale.animate="1.2" :url="foto.url" :titulo="foto.titulo"/>
           <meu-botao 
             tipo="button" 
             rotulo="REMOVER" 
